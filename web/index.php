4bbfed2c71b5428d0440e8799eb339986eb48bdc
Orbmancer <damien.bry@gmail.com>
2015-05-03 22:58:19 +0200

    Basic Twig utilisation

--- web/index.php
+++ web/index.php
@@ -1,13 +1,5 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-$app = new Silex\Application();
-
-$app->get('/', function() {
-    return 'Hello world!';
-});
-
-$app['debug'] = true;
+$app = require_once __DIR__.'/../app/app.php';
 
 $app->run();
