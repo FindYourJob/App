7d2b4ab3857d822d94affb76a40c0dcd1631a5e9
Orbmancer <damien.bry@gmail.com>
2015-05-25 18:38:45 +0200

    Added a CORS header for testing the REST routes from localhost

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,4 +4,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Base\Application();
 
+$app->after(function (Symfony\Component\HttpFoundation\Request $request, Symfony\Component\HttpFoundation\Response $response) {
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+});
+
 return $app;
